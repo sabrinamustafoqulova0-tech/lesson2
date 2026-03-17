7df11a50e0312db5c7ef9f21c29b8e42e2e65a89
Sabrina Mustafoqulova <sabrinamustafoqulova0@gmail.com>
2026-03-17 20:24:34 +0500

    lesson9

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,72 @@
-import { Button } from '@mui/material'
 import React from 'react'
+import { RightOutlined } from '@ant-design/icons';
 
-const App = () => {
+import { DownOutlined, EllipsisOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Dropdown, message, Space, Tooltip } from 'antd';
+
+
+
+
+const handleButtonClick = (e) => {
+  message.info('Click on left button.');
+  console.log('click left button', e);
+}
+const handleMenuClick = e => {
+  message.info('Click on menu item.');
+  console.log('click', e);
+};
+const items = [
+  {
+    label: '1st menu item',
+    key: '1',
+    icon: <UserOutlined />,
+  },
+  {
+    label: '2nd menu item',   
+    key: '2',
+    icon: <UserOutlined />,
+  },
+  {
+    label: '3rd menu item',
+    key: '3',
+    icon: <UserOutlined />,
+    danger: true,
+  },
+  {
+    label: '4rd menu item',
+    key: '4',
+    icon: <UserOutlined />,
+    danger: true,
+    disabled: true,
+  },
+];
+
+const menuProps = {
+  items,
+  onClick: handleMenuClick,
+};
+
+
+function App() {
+
   return (
-    <div>
+    <>
+    <div className='flex gap-5 m-[20px]'>
+      <Button type="primary">Нажми меня</Button>
+      <Button>
+      <RightOutlined />
+      </Button>
+         <Space.Compact>
+      <Button onClick={handleButtonClick} danger>
+        Danger
+      </Button>
+      <Dropdown menu={menuProps} placement="bottomRight">
+        <Button icon={<EllipsisOutlined />} danger />
+      </Dropdown>
+    </Space.Compact>
     </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
