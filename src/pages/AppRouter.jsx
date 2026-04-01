87dc10be26c966b23ef9b3ffd90e4c026da8096b
Sabrina Mustafoqulova <sabrinamustafoqulova0@gmail.com>
2026-04-01 17:50:04 +0500

    lesson18

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router'; // или 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router'; 
 import App from '../App';
 import Info from './Info';
 
